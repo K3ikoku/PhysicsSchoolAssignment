c372251389c8bae10fc33c5e3210d9ef99cb1d0f
GLaDOS <wall.gustaf@gmail.com>
2017-05-19 00:12:13 +0200

    no message

---
+++ PhysicsSchoolAssignment/Assets/Scripts/CameraController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Vector2 mouseLook;
+    Vector2 smoothingVector;
+
+    [SerializeField] private float sensitivity = 5.0f;
+    [SerializeField] private float smoothing = 2.0f;
+    [SerializeField] private GameObject grenadeType;
+    [SerializeField] private float throwCooldown = 5.0f;
+    [SerializeField] private float throwForce = 10.0f;
+
+
+    GameObject character;
+    private float attackTimer = 0;
+    // Use this for initialization
+    void Start()
+    {
+        character = this.transform.parent.gameObject;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        smoothingVector.x = Mathf.Lerp(smoothingVector.x, mouseDelta.x, smoothing * Time.deltaTime);
+        smoothingVector.y = Mathf.Lerp(smoothingVector.y, mouseDelta.y, smoothing * Time.deltaTime);
+        mouseLook += smoothingVector;
+
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -90.0f, 90.0f);
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (Input.GetButton("Fire1") && attackTimer <= 0)
+            ThrowGrenade();
+
+        if (attackTimer > 0)
+            attackTimer -= Time.deltaTime;
+    }
+
+
+    void ThrowGrenade()
+    {
+        attackTimer = throwCooldown;
+        GameObject grenade = Instantiate(grenadeType) as GameObject;
+        grenade.transform.position = transform.position + transform.forward;
+        Rigidbody grenadeBody = grenade.GetComponent<Rigidbody>();
+        grenadeBody.AddForce(transform.forward * throwForce);
+    }
+}
---
+++ PhysicsSchoolAssignment/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 10.0f;
+    [SerializeField] private float gravity = 10.0f;
+    [SerializeField] private float maxVelocityChange = 10.0f;
+    [SerializeField] private bool canJump = true;
+    [SerializeField] private float jumpHeight = 2.0f;
+
+    [SerializeField] private bool grounded = false;
+
+    private Rigidbody body;
+    private Quaternion rotation;
+
+    // Use this for initialization
+    void Awake()
+    {
+        body = gameObject.GetComponent<Rigidbody>();
+        body.freezeRotation = true;
+        body.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (grounded)
+        {
+            //Calculate how fast character is should move
+            Vector3 targetMoveSpeed = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            targetMoveSpeed = transform.TransformDirection(targetMoveSpeed);
+            targetMoveSpeed *= moveSpeed;
+
+            // Apply force
+            Vector3 currentSpeed = body.velocity;
+            Vector3 speedChange = (targetMoveSpeed - currentSpeed);
+            speedChange.x = Mathf.Clamp(speedChange.x, -maxVelocityChange, maxVelocityChange);
+            speedChange.z = Mathf.Clamp(speedChange.z, -maxVelocityChange, maxVelocityChange);
+            speedChange.y = 0;
+
+            body.AddForce(speedChange, ForceMode.VelocityChange);
+
+            if (canJump == true && Input.GetButton("Jump"))
+                body.velocity = new Vector3(currentSpeed.x, CalculateJumpSpeed(), currentSpeed.z);
+        }
+
+        body.AddForce(new Vector3(0, -gravity * body.mass, 0));
+
+        grounded = false;
+
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        grounded = true;
+    }
+
+    float CalculateJumpSpeed()
+    {
+        return Mathf.Sqrt(2 * jumpHeight * gravity);
+    }
+
+}
